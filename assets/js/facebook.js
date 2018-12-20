5c60c4e1cbc584ae5394cfee34ab907e1b72b39a
Adam Prasetia <Adam Prasetia@JKTM1KCMITASL10.kompas.com>
2018-12-21 03:20:36 +0700

    fix login

--- assets/js/facebook.js
+++ assets/js/facebook.js
@@ -1,70 +1,8 @@
-window.fbAsyncInit = function() {
-    FB.init({
-        // appId      : '2107385236164859', // development
-        // appId      : '2100687543493680', // production
-        appId      : '538862216589128',
-        cookie     : true,
-        xfbml      : true,
-        version    : 'v2.8'
-    });
-    FB.getLoginStatus(function(response) {
-        statusChangeCallback(response);
-    });
-    
-    // FB.Event.subscribe('auth.authResponseChange', checkLoginState);
-}
-  
-function checkLoginState(event) {
-  if (event.authResponse) {
-    // User is signed-in Facebook.
-    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
-      unsubscribe();
-      // Check if we are already signed-in Firebase with the correct user.
-      if (!isUserEqual(event.authResponse, firebaseUser)) {
-        // Build Firebase credential with the Facebook auth token.
-        var credential = firebase.auth.FacebookAuthProvider.credential(event.authResponse.accessToken);
-        // Sign in with the credential from the Facebook user.
-        firebase.auth().signInWithCredential(credential).then(function(){
-            FB.getLoginStatus(function(response) {
-                statusChangeCallback(response);
-            });            
-        }).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // The email of the user's account used.
-          var email = error.email;
-          // The firebase.auth.AuthCredential type that was used.
-          var credential = error.credential;
-          // ...          
-        });
-      } else {
-        // User is already signed-in Firebase with the correct user.
-      }
-    });
-  } else {
-    // User is signed-out of Facebook.
-    firebase.auth().signOut();
-  }
-}
-
-function isUserEqual(facebookAuthResponse, firebaseUser) {
-  if (firebaseUser) {
-    var providerData = firebaseUser.providerData;
-    for (var i = 0; i < providerData.length; i++) {
-      if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
-          providerData[i].uid === facebookAuthResponse.userID) {
-        // We don't need to re-auth the Firebase connection.
-        return true;
-      }
-    }
-  }
-  return false;
-}
-
 function statusChangeCallback(response) {
     if (response.status === 'connected') {
+        console.log('Welcome!  Fetching your information.... ');
         FB.api('/me', {fields: "id,name,picture"}, function(response) {
+            console.log('connected', response)
             if (response && response.id && typeof response.id !== 'undefined') {                
                 firebase.database().ref(room+'/player/'+response.id).update({
                     id:response.id,
@@ -72,6 +10,7 @@ function statusChangeCallback(response) {
                     picture:response.picture.data.url,
                 }).then(function(){
                     me = response;
+                    console.log('me', me)
                     firebase.database().ref(room+'/player/'+response.id).update({
                         sync:new Date().getUTCMilliseconds()
                     });
@@ -84,3 +23,32 @@ function statusChangeCallback(response) {
         document.getElementById('status').innerHTML = 'Silakan login terlebih dahulu.';
     }
 }
+
+function checkLoginState() {
+    FB.getLoginStatus(function(response) {
+      statusChangeCallback(response);
+    });
+}
+
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '538862216589128',
+        cookie     : true,  // enable cookies to allow the server to access 
+        xfbml      : true,  // parse social plugins on this page
+        version    : 'v2.8' // use graph api version 2.8
+    });
+
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+
+  };
+
+  // Load the SDK asynchronously
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
---
+++ assets/js/facebook_old.js
@@ -0,0 +1,90 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        // appId      : '2107385236164859', // development
+        // appId      : '2100687543493680', // production
+        appId      : '538862216589128',
+        cookie     : true,
+        xfbml      : true,
+        version    : 'v2.8'
+    });
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    });
+    
+    // FB.Event.subscribe('auth.authResponseChange', checkLoginState);
+}
+  
+function checkLoginState(event) {
+  if (event.authResponse) {
+    // User is signed-in Facebook.
+    var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+      unsubscribe();
+      // Check if we are already signed-in Firebase with the correct user.
+      if (!isUserEqual(event.authResponse, firebaseUser)) {
+        // Build Firebase credential with the Facebook auth token.
+        var credential = firebase.auth.FacebookAuthProvider.credential(event.authResponse.accessToken);
+        // Sign in with the credential from the Facebook user.
+        firebase.auth().signInWithCredential(credential).then(function(){
+            FB.getLoginStatus(function(response) {
+                statusChangeCallback(response);
+            });            
+        }).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // The email of the user's account used.
+          var email = error.email;
+          // The firebase.auth.AuthCredential type that was used.
+          var credential = error.credential;
+          // ...          
+        });
+      } else {
+        // User is already signed-in Firebase with the correct user.
+      }
+    });
+  } else {
+    // User is signed-out of Facebook.
+    firebase.auth().signOut();
+  }
+}
+
+function isUserEqual(facebookAuthResponse, firebaseUser) {
+  if (firebaseUser) {
+    var providerData = firebaseUser.providerData;
+    for (var i = 0; i < providerData.length; i++) {
+      if (providerData[i].providerId === firebase.auth.FacebookAuthProvider.PROVIDER_ID &&
+          providerData[i].uid === facebookAuthResponse.userID) {
+        // We don't need to re-auth the Firebase connection.
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+function statusChangeCallback(response) {
+    console.log('statusChangeCallback')
+    if (response.status === 'connected') {
+        console.log('connected')
+        FB.api('/me', {fields: "id,name,picture"}, function(response) {
+            console.log('connected', response)
+            if (response && response.id && typeof response.id !== 'undefined') {                
+                firebase.database().ref(room+'/player/'+response.id).update({
+                    id:response.id,
+                    name:response.name,
+                    picture:response.picture.data.url,
+                }).then(function(){
+                    me = response;
+                    console.log('me', me)
+                    firebase.database().ref(room+'/player/'+response.id).update({
+                        sync:new Date().getUTCMilliseconds()
+                    });
+                });                        
+                $('#fb-login').hide();
+                document.getElementById('status').innerHTML = 'Selamat Datang, ' + response.name + '!';
+            }
+        });        
+    } else {
+        document.getElementById('status').innerHTML = 'Silakan login terlebih dahulu.';
+    }
+}
