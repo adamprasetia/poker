651382aa7cbe2f1934798235230350284f11fb25
Adam Prasetia <Adam Prasetia@JKTM1KCMITASL10.kompas.com>
2020-04-03 06:57:23 +0700

    fix

---
+++ assets/js/auth.js
@@ -0,0 +1,32 @@
+function login()
+{
+    var name = prompt("Please enter your name:", "");
+    var data = {
+        id:name,
+        name:name,
+        picture:'assets/img/people.jpg',
+    }
+    firebase.database().ref(room+'/player/'+name).update(data).then(function(){
+        me = data;
+        setCookie('cianjur_poker_user_login', JSON.stringify(data));
+        login_status(data);
+        firebase.database().ref(room+'/player/'+name).update({
+            sync:new Date().getUTCMilliseconds()
+        });
+    });                        
+}
+function login_status(data)
+{
+    if(data.name){
+        document.getElementById('status').innerHTML = 'Selamat Datang, ' + data.name + '!';
+        $('#btn-login').hide();
+    }else{
+        document.getElementById('status').innerHTML = 'Silakan login terlebih dahulu.';
+        $('#btn-login').show();
+    }
+}
+
+var user_login = getCookie('cianjur_poker_user_login');
+if(user_login){
+    login_status(JSON.parse(user_login));
+}
--- assets/js/helper.js
+++ assets/js/helper.js
@@ -850,3 +850,24 @@ function sendChat(){
 function encodeHTML(s) {
     return s.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/"/g, '&quot;');
 }
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
